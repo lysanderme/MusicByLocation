98bd1480bf07d9b78daef5e8f4dea53efa76f695
Lysander Myrddin-Evans <myrddin-evans/.l@etoncollege.org.uk>
2023-03-01 21:53:50 +0000

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Lysander Myrddin-Evans on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
